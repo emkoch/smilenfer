4989b066c0049206f5e319a41f8e2fdb567c5b5a
Evan Koch <evanmatthiaskoch@gmail.com>
2024-07-03 11:48:13 -0400

    Update setup.py
    email update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='smilenfer',
       version='0.1',
       description='Parametric inference from GWAS smiles',
       author='Evan Koch',
-      author_email='emkoch@bwh.harvard.edu',
+      author_email='evan_koch@hms.harvard.edu',
       packages=['smilenfer'],
       install_requires=['pandas', 'numpy', 'scipy', 'numba', 'matplotlib',
                         'seaborn', 'spycial', 'rpy2'])

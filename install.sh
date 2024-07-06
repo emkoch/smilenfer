835b424ba63a936a342972ac13d027505e2e34aa
emkoch <evanmatthiaskoch@gmail.com>
2024-07-05 23:35:49 -0400

    yml files

---
+++ install.sh
@@ -0,0 +1,2 @@
+python setup.py sdist
+pip install --no-deps ./dist/smilenfer-0.1.tar.gz
---
+++ snakemake/test_run/test_run.sh
@@ -0,0 +1,4 @@
+snakemake \
+    --snakefile ../Snakefile_trait_fits_simple \
+    --cores 1 \
+    --configfile test_run.yml
